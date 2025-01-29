6dbee11eec689e4daa4f3f0439173266fb83e648
AshokC46 <ashokchavala46@gmail.com>
2025-01-29 17:43:17 +0530

    daily frontend tasks

---
+++ Day 5/practice1.js
@@ -0,0 +1,16 @@
+let yourName = "Ashok"; // This is string.It can i written only inside double or single quotes.
+console.log(yourName);
+
+let yourAge = 22; // This is number.
+console.log(yourAge);
+
+let isStudent = true; // This is boolean
+console.log(isStudent);
+
+const courseStartedYear = 2025;
+
+
+//courseStartedYear = 2024; // it wont work because const can be declared only once.
+
+console.log(courseStartedYear);
+
